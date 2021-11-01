02cc8a9ca023e7a01e494cba49b7d926aba8f930
Billy Carnell <billy.dushime@student.moringaschool.com>
2021-11-02 01:38:26 +0300

    i have replaced the radio buttons with select option to avoid more errors

--- js/scripts.js
+++ js/scripts.js
@@ -5,68 +5,25 @@ function Pizza(size, crust,  topping) {
 }
 
 
+function getvalue(){
+    var InputtedSize = document.getElementById("size").value;
+    console.log(InputtedSize);
 
-$(document).ready(function(){
-    $("form#selection").submit(function(event){
-        event.preventDefault();
+    var InputtedCrust = document.getElementById("crust").value;
+    console.log(InputtedCrust);
 
-        function fn1(){
+    var InputtedPepproni = document.getElementById("pepproni").value;
+    console.log(InputtedPepproni);
 
-        var inputtedSize =    for (var i=0; i < document.selection.size.length; i++){
-                if(document.form.group1[i].checked){
-                    found_it = document.form.group1[i].value;
-                    console.log(found_it);
-                }
-                
-            }
+    var InputtedHam = document.getElementById("ham").value;
+    console.log(InputtedHam);
 
-        var inputtedCrust =    for (var i=0; i < document.selection.crust.length; i++){
-                if(document.form.crust[i].checked){
-                    found_it = document.form.crust[i].value;
-                    console.log(found_it);
-                }
-                
-            }
-            
-        var inputtedPepproni =   for (var i=0; i < document.selection.pepproni.length; i++){
-                if(document.form.pepproni[i].checked){
-                    found_it = document.form.pepproni[i].value;
-                    console.log(found_it);
-                }
-                
-            }
+    var InputtedChicken = document.getElementById("chicken").value;
+    console.log(InputtedChicken);
 
-        var inputtedHam =    for (var i=0; i < document.selection.ham.length; i++){
-                if(document.form.ham[i].checked){
-                    found_it = document.form.ham[i].value;
-                    console.log(found_it);
-                }
-                
-            }
-
-        var inputtedChicken =    for (var i=0; i < document.selection.chicken.length; i++){
-                if(document.form.chicken[i].checked){
-                    found_it = document.form.chicken[i].value;
-                    console.log(found_it);
-                }
-                
-            }
-
-        var inputtedBeef =    for (var i=0; i < document.selection.beef.length; i++){
-                if(document.form.beef[i].checked){
-                    found_it = document.form.beef[i].value;
-                    console.log(found_it);
-                }
-                
-            }
-        
-        }
-
-    })
-}) 
-
-//var newPizza = new Pizza(, inputtedLastName);
-
+    var InputtedBeef = document.getElementById("beef").value;
+    console.log(InputtedBeef);
+}
 
 
     
