4ad3b95f9e7a7949d2bb96029bdfdc07abd71ba0
Billy Carnell <billy.dushime@student.moringaschool.com>
2021-11-02 00:15:45 +0300

    I have used a loop function to get the value from the radio buttons

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,72 @@
-function Pizza() {
-    size = document.getElementById("size-L");
-    size = document.getElementById("size-M");
-    size = document.getElementById("size-S");
-    crust = document.getElementById("crisp");
-    crust = document.getElementById("stuffed");
-    crust = document.getElementById("glutten-free");
+function Pizza(size, crust,  topping) {
+    this.size = size;
+    this.crust = crust;
+    this.topping = topping;
+}
 
-    if (size-L.checked==true) {
-      var sizeprice = 1000
-      }
-    else if (size-M.checked==true) {
-        var sizeprice = 800
+
+
+$(document).ready(function(){
+    $("form#selection").submit(function(event){
+        event.preventDefault();
+
+        function fn1(){
+
+        var inputtedSize =    for (var i=0; i < document.selection.size.length; i++){
+                if(document.form.group1[i].checked){
+                    found_it = document.form.group1[i].value;
+                    console.log(found_it);
+                }
+                
+            }
+
+        var inputtedCrust =    for (var i=0; i < document.selection.crust.length; i++){
+                if(document.form.crust[i].checked){
+                    found_it = document.form.crust[i].value;
+                    console.log(found_it);
+                }
+                
+            }
+            
+        var inputtedPepproni =   for (var i=0; i < document.selection.pepproni.length; i++){
+                if(document.form.pepproni[i].checked){
+                    found_it = document.form.pepproni[i].value;
+                    console.log(found_it);
+                }
+                
+            }
+
+        var inputtedHam =    for (var i=0; i < document.selection.ham.length; i++){
+                if(document.form.ham[i].checked){
+                    found_it = document.form.ham[i].value;
+                    console.log(found_it);
+                }
+                
+            }
+
+        var inputtedChicken =    for (var i=0; i < document.selection.chicken.length; i++){
+                if(document.form.chicken[i].checked){
+                    found_it = document.form.chicken[i].value;
+                    console.log(found_it);
+                }
+                
+            }
+
+        var inputtedBeef =    for (var i=0; i < document.selection.beef.length; i++){
+                if(document.form.beef[i].checked){
+                    found_it = document.form.beef[i].value;
+                    console.log(found_it);
+                }
+                
+            }
+        
         }
-    else if (size-S.checked==true) {
-          var sizeprice = 500
-          }
 
-    if (crisp.checked==true) {
-        var crustprice = 100
-         }
-    else if (stuffed.checked==true) {
-        var crustprice = 150
-         }
-    else if (glutten-free.checked==true) {
-        var crustprice = 100
-         }
+    })
+}) 
 
-         document.write("total is " sizeprice + crustprice) 
+//var newPizza = new Pizza(, inputtedLastName);
 
-         Pizza()
-         
-  }
+
+
+    
