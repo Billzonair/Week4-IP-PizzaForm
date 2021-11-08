bb32b9cb6dd266d66605fc6ba90b3ad9e3f25ffa
Billy Carnell <billy.dushime@student.moringaschool.com>
2021-11-08 10:35:39 +0300

    i fixed the problem of my website reloading after the submit button is clicked by deleting the form tags

--- js/scripts.js
+++ js/scripts.js
@@ -12,11 +12,29 @@ function Pizza(order_no, size, crust, pepproni, ham, chicken, beef, total) {
 
 
 Pizza.prototype.myOrderDetails = function() {
-  console.log("Your order has been received for a" + this.size "pizza with" + this.pepproni + this.ham + this.chicken + this.beef + "and a" + this.crust + "crust")
+  console.log("Your order has been received for a" + this.size + "pizza with" + this.pepproni + this.ham + this.chicken + this.beef + "and a" + this.crust + "crust")
 }
 
 $(document).ready(function() {
   $('#orderButton').click(function() {
-    
+    var sizeOfPizza = $('#pizzaSize option:selected').val();
+    var crustOfPizza = $('#pizzaCrust option:selected').val();
+    var pepproniOfPizza = $('#pizzaPepproni option:selected').val();
+    var hamOfPizza = $('#pizzaHam option:selected').val();
+    var chickenOfPizza = $('#pizzaChicken option:selected').val();
+    var beefOfPizza = $('#pizzaBeef option:selected').val();
+
+    var total = parseInt(sizeOfPizza) + parseInt(crustOfPizza) + parseInt(pepproniOfPizza) + parseInt(hamOfPizza) + parseInt(chickenOfPizza) + parseInt(beefOfPizza);
+    var order = 1;
+    var grandTotal = 0;
+
+    $('#orderNo').html(order);
+    $('#Size').html(sizeOfPizza);
+    $('#Crust').html(crustOfPizza);
+    $('#Pepproni').html(pepproniOfPizza);
+    $('#Ham').html(hamOfPizza);
+    $('#Chicken').html(chickenOfPizza);
+    $('#Beef').html(beefOfPizza);
+    $('#Total').html(total);
   })
-})
+});
